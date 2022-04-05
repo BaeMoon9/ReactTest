7ba18589003bccdff5880e35749e9a70c065d1e9
bmgbm9 <bmgbm9@naver.com>
2022-04-05 23:21:51 +0900

    20220405

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Component } from "react";
-import ValidationSample from "./ValidationSample";
+import { Component } from 'react';
+import ScrollBox from './ScrollBox';
+import ValidationSample from './ValidationSample';
 
 class App extends Component {
-  render() {
-    return <ValidationSample />;
-  }
+	render() {
+		return (
+			<div>
+				<ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+				<button onclick={() => this.ScrollBox.ScrollToBottom()}>맨밑으로</button>
+			</div>
+		);
+	}
 }
 
 export default App;
