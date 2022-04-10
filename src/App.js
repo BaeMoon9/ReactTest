5943d03c81f5af430afbb8bc420e5c9ebdefa1c5
BaeMoon9 <bmgbm9@naver.com>
2022-04-10 22:44:21 +0900

    20220410

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
-import ValidationSample from "./ValidationSample";
+import InterationSample from "./InterationSample";
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return <InterationSample />;
   }
 }
+//164p 할차례
 
 export default App;
